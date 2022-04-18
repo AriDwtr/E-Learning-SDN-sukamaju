776eaeb4f7f3235c3b1a52ab5faa60b7b080cbfc
Ari Dwiantoro <aridwiantoro09@gmail.com>
2022-04-18 16:44:09 +0700

    new db

--- elearning.sql
+++ elearning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2022 at 06:14 PM
+-- Generation Time: Apr 18, 2022 at 11:43 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -42,10 +42,59 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-(7, '2022-04-05-142550', 'App\\Database\\Migrations\\TblStaff', 'default', 'App', 1649606449, 1),
-(8, '2022-04-05-162612', 'App\\Database\\Migrations\\TblKelas', 'default', 'App', 1649606449, 1),
-(9, '2022-04-06-222714', 'App\\Database\\Migrations\\TblSiswa', 'default', 'App', 1649606449, 1),
-(10, '2022-04-09-030125', 'App\\Database\\Migrations\\TblPelajaraan', 'default', 'App', 1649606449, 1);
+(12, '2022-04-05-142550', 'App\\Database\\Migrations\\TblStaff', 'default', 'App', 1650097393, 1),
+(13, '2022-04-05-162612', 'App\\Database\\Migrations\\TblKelas', 'default', 'App', 1650097393, 1),
+(14, '2022-04-06-222714', 'App\\Database\\Migrations\\TblSiswa', 'default', 'App', 1650097393, 1),
+(15, '2022-04-09-030125', 'App\\Database\\Migrations\\TblPelajaraan', 'default', 'App', 1650097393, 1),
+(16, '2022-04-14-211153', 'App\\Database\\Migrations\\TblJadwalPelajaran', 'default', 'App', 1650097393, 1),
+(17, '2022-04-16-162708', 'App\\Database\\Migrations\\TblAbsensi', 'default', 'App', 1650126632, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_absensi`
+--
+
+CREATE TABLE `tbl_absensi` (
+  `id_absensi` int(5) UNSIGNED NOT NULL,
+  `id_jadwal` int(5) NOT NULL,
+  `id_siswa` int(5) NOT NULL,
+  `waktu_absensi` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_absensi`
+--
+
+INSERT INTO `tbl_absensi` (`id_absensi`, `id_jadwal`, `id_siswa`, `waktu_absensi`) VALUES
+(5, 2, 1, '2022-04-16 12:36:57'),
+(6, 5, 1, '2022-04-16 12:37:27'),
+(7, 2, 1, '2022-04-16 12:38:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_jadwal_pelajaran`
+--
+
+CREATE TABLE `tbl_jadwal_pelajaran` (
+  `id_jadwal` int(5) UNSIGNED NOT NULL,
+  `id_pelajaran` int(5) NOT NULL,
+  `judul_materi` varchar(255) NOT NULL,
+  `ringkas_materi` text DEFAULT NULL,
+  `link_zoom` text DEFAULT NULL,
+  `file_upload` text DEFAULT NULL,
+  `tanggal_jadwal` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_jadwal_pelajaran`
+--
+
+INSERT INTO `tbl_jadwal_pelajaran` (`id_jadwal`, `id_pelajaran`, `judul_materi`, `ringkas_materi`, `link_zoom`, `file_upload`, `tanggal_jadwal`) VALUES
+(2, 2, 'pertemuan 1', 'IPA adalah ilmu yang mempelajari segala sesutu yang berada di sekitar kita baik yang\r\nhidup maupun yang mati.\r\nDi dalam menemukan konsep-konsep IPA, maka para ilmuwan melakukan kegiatan\r\npenyelidikan yang disebut observasi (pengamatan). IPA berkembang melalui proses penelitian\r\nyang dilakukan oleh ilmuwan.\r\nPenelitian yang dilakukan ilmuwan harus melalui langkah-langkah yang terencana dan\r\nsistematis untuk memperoleh informasi yang dapat dipertanggungjawabkan secara ilmiah.\r\nLangkah-langkah yang harus dilakukan dalam melakukan penelitian tersebut dinamakan metode\r\nilmiah.                        ', 'https://us05web.zoom.us/j/89515421751?pwd=cW5wRHFLRndhbXZaUitQMmRYQm1Gdz09', NULL, '2022-04-16'),
+(4, 2, 'pertemuan 2', '<strong style=\"margin: 0px; padding: 0px; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">Lorem Ipsum</strong><span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>                        ', 'https://us05web.zoom.us/j/89515421751?pwd=cW5wRHFLRndhbXZaUitQMmRYQm1Gdz09', '1650113181_8df1d03d14aa4b6a561c.pdf', '2022-04-17'),
+(5, 1, 'pertemuan 1', '<strong style=\"margin: 0px; padding: 0px; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">Lorem Ipsum</strong><span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>                        ', 'https://us05web.zoom.us/j/89515421751?pwd=cW5wRHFLRndhbXZaUitQMmRYQm1Gdz09', '1650123642_f64b271f99a441284564.xlsx', '2022-04-16');
 
 -- --------------------------------------------------------
 
@@ -63,8 +112,7 @@ CREATE TABLE `tbl_kelas` (
 --
 
 INSERT INTO `tbl_kelas` (`id_kelas`, `kelas`) VALUES
-(1, 'kelas 1 a'),
-(2, 'kelas 2 a');
+(1, 'kelas 1 a');
 
 -- --------------------------------------------------------
 
@@ -84,8 +132,8 @@ CREATE TABLE `tbl_pelajaran` (
 --
 
 INSERT INTO `tbl_pelajaran` (`id_pelajaran`, `id_kelas`, `id_staff`, `pelajaran`) VALUES
-(2, 2, 2, 'IPAS'),
-(3, 1, 2, 'Bahasa Indonesia');
+(1, 1, 3, 'Agama'),
+(2, 1, 4, 'IPA');
 
 -- --------------------------------------------------------
 
@@ -106,6 +154,13 @@ CREATE TABLE `tbl_siswa` (
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(225) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`id_siswa`, `nisn`, `id_kelas`, `nama_siswa`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `agama`, `foto`, `username`, `password`) VALUES
+(1, '51760', 1, 'putri tringgani', 'perempuan', NULL, NULL, NULL, NULL, 'putri', '$2y$10$IftzfOzxFAcoEtVRFUk1CeXbtrd.BUQ8p/49YMhPKtGSYo5v5K/We');
 
 -- --------------------------------------------------------
 
@@ -132,8 +187,9 @@ CREATE TABLE `tbl_staff` (
 --
 
 INSERT INTO `tbl_staff` (`id_staff`, `nip`, `nama_pegawai`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `agama`, `foto`, `username`, `password`, `tipe`) VALUES
-(1, '1671050707970003', 'ari dwiantoro', 'laki-laki', NULL, NULL, NULL, NULL, 'admin', '$2y$10$ssVOCNbV7qoUaE3rX5liree511Ja2TNCyvdUJAuJj/w56lnSKYpOy', 'admin'),
-(2, '15261827010', 'rizki ratih purwasih', 'perempuan', NULL, NULL, NULL, NULL, 'ratih', '$2y$10$I7RPcyJQXKbroqPnQZSJ0./0rSn/ZguHtVoWtjEP5uQ/g6A0eg6Je', 'guru');
+(2, '0000000000000', 'admin', 'laki-laki', NULL, NULL, NULL, NULL, 'admin', '$2y$10$3KTZpz7mA4rNRLXHjyYC5eivjh.nF/lhuGGZaItE45P6rH4kXdEQS', 'admin'),
+(3, '16710506070797', 'rizki ratih purwasih', 'perempuan', NULL, NULL, NULL, NULL, 'ratih', '$2y$10$ue57lrnxPC4FLwUf120FwOzthwHGrwG4c2iN1ISJBdJ.HbpHZL7T2', 'guru'),
+(4, '517890890', 'Rama', 'laki-laki', NULL, NULL, NULL, NULL, 'rama', '$2y$10$Jj0nQ1CFBFA.rRu0soODCOzQruTwRQtAVYYNKA2w5FLVnG7pRezfK', 'guru');
 
 --
 -- Indexes for dumped tables
@@ -144,6 +200,18 @@ INSERT INTO `tbl_staff` (`id_staff`, `nip`, `nama_pegawai`, `jenis_kelamin`, `te
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_absensi`
+--
+ALTER TABLE `tbl_absensi`
+  ADD PRIMARY KEY (`id_absensi`);
+
+--
+-- Indexes for table `tbl_jadwal_pelajaran`
+--
+ALTER TABLE `tbl_jadwal_pelajaran`
+  ADD PRIMARY KEY (`id_jadwal`);
 
 --
 -- Indexes for table `tbl_kelas`
@@ -177,31 +245,43 @@ ALTER TABLE `tbl_staff`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tbl_absensi`
+--
+ALTER TABLE `tbl_absensi`
+  MODIFY `id_absensi` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_jadwal_pelajaran`
+--
+ALTER TABLE `tbl_jadwal_pelajaran`
+  MODIFY `id_jadwal` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_kelas`
 --
 ALTER TABLE `tbl_kelas`
-  MODIFY `id_kelas` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_kelas` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_pelajaran`
 --
 ALTER TABLE `tbl_pelajaran`
-  MODIFY `id_pelajaran` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pelajaran` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_siswa`
 --
 ALTER TABLE `tbl_siswa`
-  MODIFY `id_siswa` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_siswa` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_staff`
 --
 ALTER TABLE `tbl_staff`
-  MODIFY `id_staff` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_staff` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
